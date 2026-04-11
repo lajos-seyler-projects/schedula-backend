a6c94508308206b4c9c3ae2d952a9ed119000e1e
Lajos Seyler <lajosseyler@protonmail.com>
2026-04-11 03:29:16 -0400

    feat: add users app with custom User model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
