ba18222cbd57adafcb08303df2a38d330f47fd15
Lajos Seyler <lajosseyler@protonmail.com>
2026-04-14 15:15:13 -0400

    feat: configure default pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -214,6 +214,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissions"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 # django-cors-headers
